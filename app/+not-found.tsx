f9fbef5b60d1ccec398d4a89346fb5ce6b31e927
moraiskaua <moraiskaua03@gmail.com>
2024-08-26 20:43:26 -0300

    chore: install fonts

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,16 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+      <View style={styles.container}>
+        <Text>Essa tela não existe.</Text>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <Text>Ir para o início!</Text>
         </Link>
-      </ThemedView>
+      </View>
     </>
   );
 }
