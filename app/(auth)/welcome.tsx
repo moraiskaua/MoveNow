17b3af00995346b3d0f2dbe62e76d3d231fc8371
moraiskaua <moraiskaua03@gmail.com>
2024-08-26 21:57:39 -0300

    feat: create custom button component

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,52 @@
-import { Text } from 'react-native';
+import { CustomButton } from '@/components/CustomButton';
+import { onboarding } from '@/contants';
+import { routes } from '@/contants/routes';
+import { router } from 'expo-router';
+import { useRef, useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
 
 const Welcome = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const swiperRef = useRef<Swiper>(null);
+
   return (
-    <SafeAreaView className="flex-1">
-      <Text className="text-black">Welcome Page</Text>
+    <SafeAreaView className="h-full flex items-center justify-between">
+      <TouchableOpacity
+        onPress={() => router.replace(routes.auth['sign-up'])}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
+        activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF]" />}
+        onIndexChanged={index => setActiveIndex(index)}
+      >
+        {onboarding.map(item => (
+          <View key={item.id} className="flex items-center justify-center">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+
+      <CustomButton title="Próximo" className="w-11/12 mt-10" />
     </SafeAreaView>
   );
 };
